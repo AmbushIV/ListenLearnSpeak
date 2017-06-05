f746aa09889b320439633fc4973a4f8119e88645
AmbushIV <ionut.demeterca94@gmail.com>
2017-06-05 19:05:15 +0300

    Initial Commit

---
+++ ListenLearnSpeak/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ListenLearnSpeak
+//
+//  Created by Ionut Demeterca on 05/06/2017.
+//  Copyright © 2017 Ionut Demeterca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
